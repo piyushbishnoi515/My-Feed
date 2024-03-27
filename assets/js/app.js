fb82ef35a1ec53ce2f66eb95de871831c1936ecd
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-03-27 15:43:27 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// const menuicon = document.querySelector(".menuicon");
+// const menulist = document.querySelector(".menulist");
+
+// menulist.addEventListener("click", function () {
+//     menulist.classList.toggle("showlist");
+// });
+
+const blackbox = document.querySelector(".black-box");
+const blanckdiv = document.querySelector(".blanck-div");
+
+blanckdiv.addEventListener("click", function () {
+    blackbox.classList.toggle("dot-right");
+})
