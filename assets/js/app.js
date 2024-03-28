dc7b1a5f4388084e7844cc49e9b0eb4aca703902
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-03-28 09:56:40 +0530

    js update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
-// const menuicon = document.querySelector(".menuicon");
-// const menulist = document.querySelector(".menulist");
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
 
-// menulist.addEventListener("click", function () {
-//     menulist.classList.toggle("showlist");
-// });
+menulist.addEventListener("click", function () {
+    menulist.classList.toggle("showlist");
+});
 
 const blackbox = document.querySelector(".black-box");
 const blanckdiv = document.querySelector(".blanck-div");
