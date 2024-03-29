921bf3c11bd0fb8808faf4d88fb633c7f2007b33
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-03-29 10:34:52 +0530

    updates done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 const menuicon = document.querySelector(".menuicon");
 const menulist = document.querySelector(".menulist");
 
-menulist.addEventListener("click", function () {
+
+menuicon.addEventListener("click", function () {
     menulist.classList.toggle("showlist");
-});
+})
 
 const blackbox = document.querySelector(".black-box");
 const blanckdiv = document.querySelector(".blanck-div");
